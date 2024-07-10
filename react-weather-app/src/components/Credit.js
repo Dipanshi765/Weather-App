a95e381b67ccb4c7f058b96974ac7c41f5eca6b1
Dipanshi <dipanshi.diwakar765@gmail.com>
2024-07-10 21:02:34 +0530

    first commit

---
+++ react-weather-app/src/components/Credit.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Made with 🤍 by Dipanshi Diwakar.
+      
+    </p>
+  </div>
+  );
+}
